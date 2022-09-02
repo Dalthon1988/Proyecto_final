2ec46761dd50b1d0cdb052714b1a441d55365649
Dalthon1988 <dalthon1988@gmail.com>
2022-09-02 09:33:06 -0300

    se agregan las rutas al template base

--- social/urls.py
+++ social/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('profile/', views.profile,name='profile'),
     path('register/',views.register,name='register'),
     path('login/',LoginView.as_view(template_name='social/login.html'),name='login'),
-    path('logout/',LogoutView.as_view(template_name='social/logout.html'),name='login'),
+    path('logout/',LogoutView.as_view(template_name='social/logout.html'),name='logout'),
 
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
